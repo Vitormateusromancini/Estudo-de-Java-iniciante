4e991dbc9a51bae495bf197aa32012e56ccc93cc
Vitor Mateus Romancini do Amaral <77472862+Vitormateusromancini@users.noreply.github.com>
2023-10-31 10:52:47 -0300

    Create sintaxe e variaveis.java

---
+++ sintaxe e variaveis.java
@@ -0,0 +1,31 @@
+//Programação em java - miolo 
+
+class Meuprograma {
+  public static void main(String[] args) {
+
+    //miolo do programa começa aqui 
+
+    System.out.println("Meu primeiro programa");
+
+    //fim do miolo do programa 
+  }
+
+}
+
+//variaveis em java e como declarar elas
+
+//	declara	a	idade.
+int	idade;
+idade	=	15;
+//	imprime	a	idade.
+System.out.println(idade);
+
+int quatro = 2 + 2 
+
+int um = 5 % 2; // 5 dividido por 2 dá 2 e tem resto 1 e o % pega o resto da divisão inteira 
+
+// double, long e float são pontos flutuantes 
+
+double pi = 3.14; 
+
+boolean menordeidade = idade < 18; 
