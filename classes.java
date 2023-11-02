08cd66d526317a82b89f2585e57285590dd6e9dd
Vitor Mateus Romancini do Amaral <77472862+Vitormateusromancini@users.noreply.github.com>
2023-11-02 14:08:47 -0300

    Create classes.java

---
+++ classes.java
@@ -0,0 +1,24 @@
+//classe conta 
+class Conta	{
+ int numero;
+ String titular;
+ double saldo;
+  //	..
+				}
+
+// construir conta 
+
+class Programa	{
+ public static void main(String[] args) {
+  new	Conta();				
+		}
+}
+
+// acessanto a classe conta 
+
+class Programa {
+  public static void main(String[] args) {
+   Conta minhaCont; 
+   minhaConta = new Conta();
+  }
+}
